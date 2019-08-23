06f7c878b58cf93ea47a2d6093546e047dd0fab0
Makoto Ri <nano.osamu@gmail.com>
2019-08-23 17:11:33 +0700

    Added Web Control

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 const chatbot = require('./lib/bot');
+const path = require('path');
 
 // Construct Chatbot Instances
 let bot = new chatbot();
@@ -34,6 +35,30 @@ app.post('/callback', line.middleware(config), (req, res) => {
       res.status(500).end();
     });
 });
+app.get('/getDatabase', (req, res) => {
+  res.sendFile(path.join(__dirname, './database.txt'));
+  res.setHeader('Content-type', 'application/json');
+  return;
+});
+
+app.get('/database', (req, res) => {
+  res.send(bot.currentDatabase);
+  return 200;
+});
+
+app.use(require('body-parser').json());
+app.post('/updateDatabase', (req, res) => {
+  // If there is no task send
+  if(Object.keys(req.body) === 0) return;
+  // Otherwise update the database
+  console.log(req.body);
+  bot._updateDatabase(req.body);
+ 
+  return 200;
+});
+
+
+app.use('/', express.static('manager'));
 
 // event handler
 function handleEvent(event) {
---
+++ manager/classes/main.js
@@ -0,0 +1,162 @@
+class Main{
+    constructor(){ 
+        this.taskContainer = {};
+        this.taskContainer.class = "dateBox";
+        this.taskContainer.construct = ['deadline', 'taskBox'];
+        this.taskClass = 'task';
+        this.database = {};
+
+        this._initDatabase();
+
+        // Elements
+        this.scheduleInterface = document.querySelector('div#schedule');
+        this.textbox = document.querySelector('input#addEvent');
+        this.calendar = document.querySelector('input#date');
+        this.button = document.querySelector('input#add');
+
+        this.calendar.min = Date.now()
+    }
+    
+    async _initDatabase(){
+        // Cleanup and database fetching
+        let tmp = await fetch('/getDatabase');
+        let b = await tmp.json();
+        
+        this.database = b;
+        
+        tmp = undefined;
+        b = undefined;
+
+        this._renderDatabaseToHTML();
+
+        this.button.addEventListener('click', _ => {
+            if(this.calendar.value === '' || this.textbox.value === '') {
+                alert('Fill in the blanks!');
+                return;
+            };
+            let date = new Date(this.calendar.value).toDateString().split(' ');
+            date.shift();
+
+            // Swap Month and Day
+            let tmp = date[0];
+            date[0] = date[1]
+            date[1] = tmp;
+            tmp = undefined;
+
+            date = date.join(' ');
+
+            // Add to database
+            if(this.database[date] === undefined){
+                this.database[date] = [];
+            }
+
+            this.database[date].push(this.textbox.value);
+            this._renderDatabaseToHTML();
+            this._updateDatabaseToServer();
+        });
+
+    }
+
+    _parseDatabaseToHTML(){
+        let deadlines = Object.keys(this.database).sort();
+        let dataBoxesComponents = [];
+        
+        // Initalize deadline boxes ands tasks
+        for(let date of deadlines){
+            let tmp = document.createElement('div');
+            tmp.classList.toggle(this.taskContainer.class);
+            
+            let deadlineTitle = document.createElement('div');
+            deadlineTitle.classList.toggle(this.taskContainer.construct[0]);
+            deadlineTitle.textContent = date;
+            
+            // Set the deadline title
+            tmp.appendChild(deadlineTitle);
+            
+            let taskBox = this._parseToTaskBoxComponent();
+            // Input the tasks on that day
+            for(let task of this.database[date]){
+                taskBox.appendChild(this._parseToTaskComponent(task));
+            }
+            tmp.appendChild(taskBox);
+            dataBoxesComponents.push(tmp);
+        };
+        return dataBoxesComponents;
+    }
+    
+    _parseToTaskBoxComponent(){
+        let task = document.createElement('div');
+        task.classList.toggle(this.taskContainer.construct[1]);
+        return task;
+    }
+    _parseToTaskComponent(title){
+        let taskContainer = document.createElement('div');
+        taskContainer.classList.toggle('task');
+        
+        let task = document.createElement('div');
+        task.classList.toggle('taskTitle');
+        task.textContent = title;
+
+        let deleteBtn = document.createElement('input');
+        deleteBtn.type = "button";
+        deleteBtn.value = "Delete";
+        
+        taskContainer.appendChild(task);
+        taskContainer.appendChild(deleteBtn);
+
+        return taskContainer;
+    }
+
+    _renderDatabaseToHTML(){
+        // Scan for empty date
+        Object.keys(this.database).map(date => {
+            if(this.database[date].length === 0){
+                delete this.database[date];
+            }
+        });
+        this._updateDatabaseToServer();
+
+        if(Object.keys(this.database).length === 0){
+            this.scheduleInterface.innerHTML = "";
+            return;
+        }
+        // Clear first (lazy)
+        this.scheduleInterface.innerHTML = "";
+
+        let componentsToRender = this._parseDatabaseToHTML();
+
+        componentsToRender.map( component => {
+            console.log(component);
+            this.scheduleInterface.appendChild(component);
+        });
+
+        // Set all event listeners
+        document.querySelectorAll('input[type="Button"]').forEach( item => {
+            item.addEventListener('click', _ => {
+ 
+                let targettedDateBox = item.parentElement.parentElement.parentElement;
+                let deadline = targettedDateBox.children[0].textContent;
+                let taskName = item.parentElement.children[0].textContent;
+                
+                targettedDateBox = undefined;
+
+                // Remove item at local database and update at remote database
+                this.database[deadline].splice(this.database[deadline].indexOf(taskName), 1);
+                this._updateDatabaseToServer();
+                this._renderDatabaseToHTML();
+            });
+        });
+    }
+
+    async _updateDatabaseToServer(){
+        await fetch('/updateDatabase', {
+            method: "POST",
+            body: JSON.stringify(this.database),
+            headers: {
+                'Content-type': "application/json"
+            }
+        });
+    }
+}
+
+window.addEventListener('load', _ => new Main());
